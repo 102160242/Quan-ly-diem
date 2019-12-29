888f7e8b317b5e85a07e18158c8289de47544a22
Doan Huu Hoa <doanhuuhoa@outlook.com.vn>
2019-12-29 09:27:32 +0700

    Fix lỗi khi set/unset roles admin/teacher

--- app/Http/Resources/Course.php
+++ app/Http/Resources/Course.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\CourseClass as CourseClassResource;
 
 class Course extends JsonResource
 {
@@ -17,6 +18,7 @@ class Course extends JsonResource
         return [
             "id" => $this->id,
             "name" => $this->name,
+            "course_classes" => CourseClassResource::collection($this->whenLoaded('courseClasses')),
             "total_course_classes" => $this->courseClasses->count(),
         ];
     }
